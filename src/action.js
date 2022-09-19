f059b11c942b87ec5024aeb02a66074a066aac81
dan-frank <dan.frank.lucas@gmail.com>
2022-09-19 12:18:47 +0100

    Added file checker
    - Added a file checker to see if file exists
    
    - Added a check for a file that will fail

--- src/action.js
+++ src/action.js
@@ -1,10 +1,13 @@
 import core from "@actions/core"
 
 import { run } from "./run.js"
+import { checkFile } from "./checkFile.js"
 
 ( async () => {
     try {
         run()
+        checkFile("README.md")
+        checkFile("REQUIRED.md")
     } catch (error) {
         core.setFailed(error.message)
     }
---
+++ src/checkFile.js
@@ -0,0 +1,14 @@
+import core from "@actions/core"
+import fs from "fs"
+
+export async function checkFile(filePath) {
+  return fs.promises.access(filePath)
+    .then(() => {
+      core.info(`File ${filePath} exists`)
+      return true
+    })
+    .catch(() => {
+      core.setFailed(`File ${filePath} does not exist`)
+      return false
+    })
+}

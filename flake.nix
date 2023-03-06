63f7a768c54cbc5020b0ad1621cda336e24aebd6
Daniel Lucas <dan.frank.lucas@gmail.com>
2023-03-06 20:58:15 +0000

    Added nix flakes to manage project dependencies

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = with pkgs; pkgs.mkShell {
+          buildInputs = [
+            nodejs-18_x
+            (yarn.override { nodejs = nodejs-18_x; })
+          ];
+
+          shellHook = ''
+            if [ ! -d "node_modules" ]
+            then
+              yarn install
+            fi
+
+            yarn unused
+            yarn build
+          '';
+        };
+      });
+}
